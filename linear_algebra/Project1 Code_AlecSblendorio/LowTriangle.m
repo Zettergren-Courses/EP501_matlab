5c6a43772c668a1491d60bac4f4b8ae480087db7
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-09-30 17:25:48 -0400

    30 sept additions1

---
+++ linear_algebra/Project1 Code_AlecSblendorio/LowTriangle.m
@@ -0,0 +1,27 @@
+%% Write a forward substitution function for a lower-triangular system
+
+function [Alow,x] = LowTriangle(L,b)
+nref=length(b); %system size for reference problem
+x = zeros(nref,1);
+Alow=cat(2,L,b);
+%x(1,1) = b(1)/L(1,1);
+%x(1)=Alow(nref,nref+1)/Alow(nref,nref);
+x(1)=Alow(1,nref+1)/Alow(1,1);
+
+for i=2:nref
+    x(i,1)=(b(i)-L(i,1:i-1)*x(1:i-1,1))./L(i,i);    
+end %for 
+
+ Alow(nref) = b(nref)/L(nref,nref);
+ disp('Lower Triangular Forward Elim([Aref,bref]) = ');
+ disp(Alow);
+ disp('Forward Substitution Results =');
+ disp(x);
+
+%% Illustrate forward substitution on B using provided Matlab function
+% xsoln=tril(Alow);
+% disp('Low Triangular solution:  ');
+% disp(xsoln);
+disp('Matlab,GNU/Octave built-in solution:  ');
+disp(L\b); 
+end %function 
---
+++ linear_algebra/Project1 Code_AlecSblendorio/Project1.m
@@ -0,0 +1,53 @@
+%% Author: Alec Sblendorio 
+%% Due Date: September 28, 2020
+%% Subject: EP 501 Numerical Methods for Scientists and Engineers 
+%% Project #1 
+% Problem #1: Develop some basic elimination and substitution tools for linear equations
+% Problem #2: Elimination methods for computing matrix inverses
+% Problem #3: Numerical approaches for computing determinants
+
+%% Import test data for calibration of software
+Data1 = importdata('/Users/alecsblendorio/Documents/Projects/EP501_assignments/assignments/HW1/lowertriang_testproblem.mat');
+Data2 = importdata('/Users/alecsblendorio/Documents/Projects/EP501_assignments/assignments/HW1/testproblem.mat');
+
+A = Data2.A;
+b = Data2.b;
+b2 = Data2.b2;
+b3 = Data2.b3;
+L = Data1.L;
+bL = Data1.bL;
+
+%% Problem #1: Develop some basic elimination and substitution tools for linear equations
+%%Part A and B: Upper Triangular
+disp('%%%%%%%%%%%%%%%%%%PROBLEM #1 ANSWER BEGIN%%%%%%%%%%%%%%%%%%');
+soln = @forwardElim;
+x = soln(A,b);
+ 
+%%Part C and D: Lower Triangular
+soln2 = @LowTriangle;
+y = soln2(L,bL);
+disp('%%%%%%%%%%%%%%%%%%PROBLEM #1 ANSWER END%%%%%%%%%%%%%%%%%%');
+%% Problem #2: Elimination methods for computing matrix inverses
+disp('%%%%%%%%%%%%%%%%%%PROBLEM #2 ANSWER BEGIN%%%%%%%%%%%%%%%%%%');
+%%I was not able to complute the Inverse of the test matrix using the
+%%Gauss-Jordan Elimination method.
+%%Part A B,C, and D: Gauss-Jordan Elimination with back substitution and Inverse
+soln3 = @gaussjordanElim;
+g = soln3(A,eye(8,8));
+disp('%%%%%%%%%%%%%%%%%%PROBLEM #2 ANSWER END%%%%%%%%%%%%%%%%%%');
+%% Problem #3: Numerical approaches for computing determinants
+%%Part A and B: Create Gaussian Elimination function with Determinant
+%%solution, compare to Matlab
+disp('%%%%%%%%%%%%%%%%%%PROBLEM #3 ANSWER BEGIN%%%%%%%%%%%%%%%%%%');
+soln4 = @determinant;
+h = soln4(A,b);
+disp('%%%%%%%%%%%%%%%%%%PROBLEM #3 ANSWER END%%%%%%%%%%%%%%%%%%');
+
+%% END PROJECT #1 
+
+
+
+
+
+
+
