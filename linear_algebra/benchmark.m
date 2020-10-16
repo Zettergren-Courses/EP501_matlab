0c89517cbb6b6acd44d06e68d525429cfe60cb46
Matthew Zettergren <mattzett@gmail.com>
2020-10-16 08:45:29 -0400

    updated benchmark script for midterm

--- linear_algebra/benchmark.m
+++ linear_algebra/benchmark.m
@@ -1,4 +1,5 @@
-% Evaluate performance and scaling of Gaussian elimination by solving systems of different size
+% Evaluate performance and scaling of Gaussian elimination and Jacobi iteration
+%    by solving systems of different size and timing the solves
 
 nvals=50:50:500;
 testtimes=zeros(size(nvals));
@@ -7,8 +8,8 @@ lrep=10;     %how many times to repeat each test
 disp('Start of tests of Gaussian-elimination scaling');
 for in=1:numel(nvals)
     nlarge=nvals(in);
-    Blarge=randn(nlarge,nlarge);
-    blarge=randn(nlarge,1);
+    Blarge=diag(-1*ones(nlarge-1,1),-1)+diag(-1*ones(nlarge-1,1),1)+diag(4*ones(nlarge,1),0);    %this must be diagonally dominant or else the method won't converge
+    blarge=ones(nlarge,1);
     
     for irep=1:lrep     %benchmark will repeat the same solution several times to eliminate random variations from CPU load, etc.
         tstart=cputime;
@@ -17,11 +18,37 @@ for in=1:numel(nvals)
         tend=cputime;
         testtimes(in)=testtimes(in)+(tend-tstart)/lrep;
     end %for
-    disp([' solution for system of size ',num2str(nlarge),' takes average time ',num2str(testtimes(in)),' s']);
+    disp([' GE solution for system of size ',num2str(nlarge),' takes average time ',num2str(testtimes(in)),' s']);
 end %for
 
-figure(2);
+figure(1);
 plot(nvals,testtimes,'o','LineWidth',2,'MarkerSize',20,'MarkerFaceColor','blue')
 xlabel('system size');
 ylabel('time to solve (s)');
-title('Empirically Determined Performance of Gaussian Elimination');
+title('Empirically Determined Performance');
+
+disp('Start of tests for Jacobi iteration');
+tol=1e-9;
+testtimes=zeros(size(nvals));
+for in=1:numel(nvals)
+    nlarge=nvals(in);
+    Blarge=diag(-1*ones(nlarge-1,1),-1)+diag(-1*ones(nlarge-1,1),1)+diag(4*ones(nlarge,1),0);    %this must be diagonally dominant or else the method won't converge
+    blarge=ones(nlarge,1);
+
+    for irep=1:lrep     %benchmark will repeat the same solution several times to eliminate random variations from CPU load, etc.
+        tstart=cputime;
+        x0=randn(nlarge,1);
+        [xit,iterations]=Jacobi(x0,Blarge,blarge,tol,false);
+        tend=cputime;
+        testtimes(in)=testtimes(in)+(tend-tstart)/lrep;
+    end %for
+    disp([' JI solution for system of size ',num2str(nlarge),' takes average time ',num2str(testtimes(in)),' s']);
+end %for
+
+figure(1);
+hold on
+plot(nvals,testtimes,'^','LineWidth',2,'MarkerSize',20,'MarkerFaceColor','blue')
+xlabel('system size');
+ylabel('time to solve (s)');
+legend('Gauss elim.','Jacobi it.')
+title('Empirically Determined Performance');
