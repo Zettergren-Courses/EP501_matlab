5512c095bb438037c209873852a4bd4d63756d5e
Matthew Zettergren <mattzett@gmail.com>
2020-10-22 11:01:19 -0400

    tweak bilinear fit plotting

--- polynomials/interp_examples.m
+++ polynomials/interp_examples.m
@@ -1,3 +1,6 @@
+%% Set paths
+addpath ../linear_algebra;
+
 %% Illustration of bilinear interpolation, single interval of interest
 x=[1,2];
 y=[2,3];
@@ -11,16 +14,31 @@ fvec=f(:);
 xvec=X(:);
 yvec=Y(:);
 M=[ones(4,1),xvec(:),yvec(:),xvec(:).*yvec(:)];
-avec=M\fvec;    %could be elimination or some other method...
+[Mmod,order]=Gauss_elim(M,fvec);
+avec=backsub(Mmod(order,:));
 finterpmanual=avec(1)+avec(2)*x1+avec(3)*y1+avec(4)*x1*y1;
 
 % Matlab version
 finterp=interp2(X,Y,f,x1,y1);
 
+%Visual interpolation point:  x1,y1
+figure(1);
+imagesc(x,y,f);
+axis xy;
+xlabel('x');
+ylabel('y');
+c=colorbar;
+ylabel(c,'f(x,y)')
+hold on;
+plot(xvec,yvec,'w^','MarkerSize',15,'MarkerFaceColor','white');
+plot(x1,y1,'wo','MarkerSize',20,'MarkerFaceColor','white');
+hold off;
+
 
 %% Illustrate cubic spline approximations using Matlab functions
 x=linspace(-5,5,15);
 y=sin(x);
+figure(2);
 plot(x,y,'o','MarkerSize',20);
 
 splinedef=spline(x,y);
